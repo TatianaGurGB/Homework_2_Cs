81fa07158fd4ca9fe49db7638a1f15343ab780a0
Tatiana <Tatiana_novik@mail.ru>
2022-05-15 22:08:42 +0700

    Все 4 задачи

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит случайное 
+// трёхзначное число и удаляет вторую цифру этого числа.
+
+int b = 0;
+int c = 0;
+void TwoNumber(int a)
+{
+    if (b == a % 10){
+        Console.WriteLine(b);
+    }
+    if (c == a / 10){
+        Console.WriteLine(c); }
+    else
+    {
+        Console.WriteLine ("Число не трехзначное");
+    }
+}
+Console.WriteLine(TwoNumber(new Random().Next(100, 1000)));
